3ad23654f4e263bedfc086dfe122bc55cfadceee
Arseniy <arseniyc1306@gmail.com>
2022-03-14 17:03:18 +0300

    Animation for login & registration

--- app/src/main/java/com/example/travaler/MainActivity.kt
+++ app/src/main/java/com/example/travaler/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.travaler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.travaler.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        supportActionBar?.hide()
 
     }
 }
